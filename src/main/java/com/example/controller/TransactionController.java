7c558020fc5fd2b77d10102e4955a0eeb01c83d0
ZB <zeynepbstg@hotmail.com>
2023-04-11 22:55:42 -0400

    make transfer view is mapped

--- src/main/java/com/example/controller/TransactionController.java
+++ src/main/java/com/example/controller/TransactionController.java
@@ -1,13 +1,14 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TransactionController {
 
     @GetMapping("/make-transfer")
-    public String makeTransferVew(){
+    public String makeTransferVew(Model model){
 
         return "/transaction/make-transfer";
     }
